f9c39c72dad6b5bfef1f222219777109d97b4081
silakocakaya <67272114+silakocakaya@users.noreply.github.com>
2022-10-06 18:42:20 +0300

    sleuth zipkin

--- currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.microservice.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	Environment environment;
 	
@@ -20,6 +22,8 @@ public class CurrencyExchangeController {
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
 										@PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
 		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
